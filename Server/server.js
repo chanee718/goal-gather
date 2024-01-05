2789b7c62684804a4dcb3518e57ef9cc5f26cb02
chanee718 <chanee718@kaist.ac.kr>
2024-01-05 11:39:38 +0900

    New Project

---
+++ Server/server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '[tmdcks03]',
+  database: 'example'
+});
+
+db.connect((err) => {
+  if (err) {
+    console.log('MySQL 연결 오류:', err);
+    throw err;
+  }
+  console.log('MySQL에 연결되었습니다.');
+});
+
+// 사용자 목록을 가져오는 엔드포인트
+app.get('/users', (req, res) => {
+  db.query('SELECT * FROM tb_example', (err, result) => {
+    if (err) throw err;
+    res.json(result);
+  });
+});
+
+// 새로운 사용자 추가 엔드포인트
+app.post('/users', (req, res) => {
+  const { email, password } = req.body;
+  const query = 'INSERT INTO users (email, password) VALUES (?, ?)';
+  db.query(query, [email, password], (err, result) => {
+    if (err) throw err;
+    res.json({ message: '사용자가 추가되었습니다.', userId: result.insertId });
+  });
+});
+
+app.listen(3000, () => {
+  console.log('서버가 3000번 포트에서 실행 중입니다.');
+});
