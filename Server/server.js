80f7068f9aa44e61c2e658bc4ad719f872c21c5f
chanee718 <chanee718@kaist.ac.kr>
2024-01-09 11:30:04 +0900

    backend connect

--- Server/server.js
+++ Server/server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const db = require('./db');
 const authRoutes = require('./auth');
+const chatRoutes = require('./chat');
+const storeRoutes = require('./store');
+const gameRoutes = require('./game');
+const teamRoutes = require('./team');
 
 const app = express();
 
@@ -10,11 +14,17 @@ app.use(bodyParser.json());
 app.use(express.json()); 
 
 app.use('/auth', authRoutes);
+app.use('/chat', chatRoutes);
+app.use('/store', storeRoutes);
+app.use('/game', gameRoutes);
+app.use('/team', teamRoutes);
 
-app.get('/users', async (req, res) => {
+//특정 email을 받았을 때 그 email을 가진 user의 data를 돌려주는 query
+app.get('/users', async (req, res) => {   
+  const email = req.query.email;
   try {
-    const [rows, fields] = await db.execute('SELECT * FROM tb_example');
-    console.log('tb_examples에서 데이터 가져감!');
+    const [rows, fields] = await db.execute('SELECT * FROM users WHERE user_email = ?', [email]);
+    console.log('users에서 데이터 가져감!');
     res.json(rows);
   } catch (error) {
     console.error('쿼리 실행 중 에러:', error);
@@ -22,17 +32,6 @@ app.get('/users', async (req, res) => {
   }
 });
 
-// 새로운 사용자 추가 엔드포인트
-app.post('/users', async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    const [result] = await db.execute('INSERT INTO tb_example (email, password) VALUES (?, ?)', [email, password]);
-    res.json({ message: '사용자가 추가되었습니다.', userId: result.insertId });
-  } catch (error) {
-    console.error('쿼리 실행 중 에러:', error);
-    res.status(500).json({ error: '서버 오류' });
-  }
-});
 
 app.listen(3000, () => {
   console.log('서버가 3000번 포트에서 실행 중입니다.');
